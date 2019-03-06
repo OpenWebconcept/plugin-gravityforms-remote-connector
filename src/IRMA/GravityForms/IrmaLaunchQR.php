c84da8326ccbc8904e48a2c644dbb3fa42f69b3f
Mike van den Hoek <Mike@yard.nl>
2019-03-06 16:00:01 +0100

    (Add): added custom fields

---
+++ src/IRMA/GravityForms/IrmaLaunchQR.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace IRMA\WP\GravityForms;
+
+use GF_Field;
+
+class IrmaLaunchQR extends GF_Field {
+
+    public $type = 'IRMA-launch-QR';
+
+}
+
+?>
---
+++ src/IRMA/GravityForms/includes/class-IRMASettingsPage.php
@@ -0,0 +1,9 @@
+<?php 
+
+add_filter( 'gform_form_settings_menu', array( 'IRMASettingsPage', 'load' ), 5 );
+
+class IRMASettingsPage {
+
+}
+
+?>
