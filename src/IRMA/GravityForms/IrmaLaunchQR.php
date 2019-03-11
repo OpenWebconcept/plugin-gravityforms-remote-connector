ea13b198af72af2894b4a61e4ee2bbf36b549276
Mike van den Hoek <Mike@yard.nl>
2019-03-11 16:07:18 +0100

    (add): extended IRMA metabox fields

--- src/IRMA/GravityForms/IrmaLaunchQR.php
+++ src/IRMA/GravityForms/IrmaLaunchQR.php
@@ -8,6 +8,13 @@ class IrmaLaunchQR extends GF_Field {
 
     public $type = 'IRMA-launch-QR';
 
+    public function get_form_editor_button() {
+        return array(
+            'group' => 'advanced_fields',
+            'text'  => $this->get_form_editor_field_title(),
+        );
+    }
+    
 }
 
 ?>
