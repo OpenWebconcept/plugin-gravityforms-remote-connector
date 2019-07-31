c762e5b6ea0321800e61cdc60969f2a21b2715fc
Mike van den Hoek <mvdhoek1991@gmail.com>
2019-07-31 16:09:20 +0200

    (add): front-end view of IRMA header advanced field

--- src/IRMA/GravityForms/resources/irma-header-input.php
+++ src/IRMA/GravityForms/resources/irma-header-input.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @var int
  * @var int    $id
@@ -8,10 +9,34 @@
  */
 ?>
 <div class="ginput_container ginput_container_irma_header">
-    <nav class="navbar navbar-light bg-light">
-	  <!-- <a class="navbar-brand" href="#"> -->
-            <img alt="irma-logo" src="<?php echo $logoUrl; ?>" />
-      <!-- </a> -->
-      <?php echo $value; ?>
-    </nav>
+  <nav class="navbar navbar-light bg-light">
+    <!-- <a class="navbar-brand" href="#"> -->
+    <img alt="irma-logo-new" class="irma-logo" src="<?php echo $logoUrl; ?>" />
+    <!-- </a> -->
+    <div id="irma-login-text"><?php echo $irmaHeaderText; ?></div>
+    <button <?php echo $this->get_tabindex(); ?> class="btn btn-success gf_irma_qr" data-id="<?php echo $id; ?>" data-form-id="<?php echo $formId; ?>" data-popup="<?php echo $popup; ?>"><i class="fa fa-fw fa-user"></i><?php echo $irmaHeaderButtonLabel; ?></button>
+  </nav>
 </div>
+
+<script>
+  document.getElementsByTagName("body")[0].onload = function() {
+    // convert PHP variables into JS variables
+    let attributeFullnameID = "<?php echo $irmaHeaderAttributeFullnameID ?>";
+    let attributeBsnID = "<?php echo $irmaHeaderAttributeBsnID ?>";
+
+    //detect if BSN has changed
+    bsn = document.getElementById(attributeBsnID);
+    oldval = newval = bsn.value;
+    setInterval(function() {
+      newval = bsn.value
+      if (oldval !== newval) {
+        txt = "Ingelogd als " + document.getElementById(attributeFullnameID).value;
+        txt += " (BSN " + newval + ")";
+        document.getElementById("irma-login-text").innerHTML = txt;
+
+        oldval = newval;
+      }
+    }, 100);
+
+  };
+</script>
