6e7a406baaa88110882b060bb1afa42cd5be14ba
Mike van den Hoek <mvdhoek1991@gmail.com>
2019-07-31 16:07:49 +0200

    (add): form settings for IRMA header advanced field

--- src/IRMA/GravityForms/resources/irma-form-settings.php
+++ src/IRMA/GravityForms/resources/irma-form-settings.php
@@ -11,10 +11,42 @@
 <li class="irma_header field_setting">
 	<label class="section_label" for="irma_header_list_label">
 		<?php esc_html_e('IRMA Header text', 'irma-wp'); ?>
-		<?php gform_tooltip('irma_header'); ?>
 	</label>
 	<div class="irma_header_list">
-		<input type="text" class="irma_header_field" onchange="SetFieldProperty('irmaHeader', this.value);" />
+		<input type="text" class="irma_header_field" onchange="SetFieldProperty('irmaHeaderText', this.value);" />
+	</div>
+	<br />
+	<div class="irma_qr_popup_container">
+		<label class="section_label" for="irma_header_qr_popup">
+			<?php esc_html_e('QR Code', 'irma-wp'); ?>
+		</label>
+		<input type="checkbox" id="irma_header_qr_popup" class="irma_header_qr_popup" />
+		<label for="irma_header_qr_popup" class="inline">
+			<?php esc_html_e('Display as pop-up', 'irma-wp'); ?>
+		</label>
+	</div>
+	<br />
+	<label class="section_label" for="irma_qr_button_label">
+		<?php esc_html_e('Button label', 'irma-wp'); ?>
+	</label>
+	<div class="irma_qr_popup_container">
+		<input type="text" id="irma_header_button_label" class="irma_header_button_label" onchange="SetFieldProperty('irmaHeaderButtonLabel', this.value);" />
+	</div>
+	<br />
+	<label class="section_label" for="irma_header_attribute_fullname_id_label">
+		<?php esc_html_e('ID attribute fullname', 'irma-wp'); ?>
+		<?php gform_tooltip('irma_header_attribute_fullname_id'); ?>
+	</label>
+	<div class="irma_header_attribute_fullname_id">
+		<input type="text" class="irma_header_attribute_fullname_id" onchange="SetFieldProperty('irmaHeaderAttributeFullnameId', this.value);" />
+	</div>
+	<br />
+	<label class="section_label" for="irma_header_attribute_bns_id_label">
+		<?php esc_html_e('ID attribute BSN', 'irma-wp'); ?>
+		<?php gform_tooltip('irma_header_attribute_bsn_id'); ?>
+	</label>
+	<div class="irma_header_attribute_bsn_id">
+		<input type="text" class="irma_header_attribute_bsn_id" onchange="SetFieldProperty('irmaHeaderAttributeBsnId', this.value);" />
 	</div>
 </li>
 
